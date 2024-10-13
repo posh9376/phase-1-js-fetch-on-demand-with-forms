b4ad777ff3451f56cce4f21963b33f996eded09f
Eric Richard <reric0802@gmail.com>
2024-10-13 15:42:13 +0300

    added js to ensure working

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 const init = () => {
-  
+  let form = document.querySelector('form')
+  form.addEventListener('submit',e => {
+    e.preventDefault()
+    let input = e.target.searchByID.value
+    fetchIt(input)
+    form.reset()
+
+    function fetchIt(inpt) {
+        fetch(`http://localhost:3000/movies/${inpt}`)
+        .then(res => res.json())
+        .then(data => {
+            const title = document.querySelector("section#movieDetails h4");
+            const summary = document.querySelector("section#movieDetails p");
+            
+            title.innerText = data.title;
+            summary.innerText = data.summary;
+          });
+    }
+  })
 }
 
 document.addEventListener('DOMContentLoaded', init);
